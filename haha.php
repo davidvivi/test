7daf8f9f92b7bb79699a266b085cecee8379e53b
wuwei <646904527@qq.com>
2017-10-26 11:05:14 +0800

    修改

--- haha.php
+++ haha.php
@@ -1,9 +1,9 @@
-<?php 
-<<<<<<< HEAD
+﻿<?php 
+
 	$numb = array( array(10,15,30),array(10,15,30),array(10,15,30) ); 
   
 	echo count($numb,1);
-=======
+
 	echo "你好111,再一次修改";
->>>>>>> 9ac7230ca3d777dddfae34678792002ce787ad39
+
  ?>
